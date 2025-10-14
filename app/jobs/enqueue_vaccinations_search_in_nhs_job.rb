e372de8aa717cdefc97a4f89065670214c5d00f9
Misha Gorodnitzky <misha.gorodnitzky1@nhs.net>
2025-10-14 18:55:59 +0100

    Add rolling searches
    This allows us to ensure that patient vaccination information is
    up-to-date even if they are not in a session, by searching for patients
    that haven't been updated over the last 28 days.
    
    Jira-Issue: MAV-1995

--- app/jobs/enqueue_vaccinations_search_in_nhs_job.rb
+++ app/jobs/enqueue_vaccinations_search_in_nhs_job.rb
@@ -3,30 +3,61 @@
 class EnqueueVaccinationsSearchInNHSJob < ApplicationJob
   queue_as :immunisations_api
 
-  def perform(sessions = nil)
-    scope =
-      if sessions
-        Session.where(id: sessions.map(&:id))
-      else
-        flu = Programme.flu.sole
+  def perform(programme_types: nil)
+    programme_types ||= default_programme_types
+    programmes = Programme.where(type: programme_types)
 
-        scope =
-          Session
-            .includes(:session_dates)
-            .has_programmes([flu])
-            .where("session_dates.value >= ?", Time.zone.today)
-            .references(:session_dates)
+    patient_ids = Set.new
 
-        scope.where("sessions.send_invitations_at <= ?", 2.days.from_now).or(
-          scope.where("sessions.send_consent_requests_at <= ?", 2.days.from_now)
-        )
-      end
-
-    scope.find_each do |session|
-      ids = session.patients.pluck(:id)
-      next if ids.empty?
-
-      SearchVaccinationRecordsInNHSJob.perform_bulk(ids.zip)
+    if Flipper.enabled?(:imms_api_enqueue_session_searches)
+      patient_ids += patient_ids_session_searches(programmes:)
     end
+
+    if Flipper.enabled?(:imms_api_enqueue_rolling_searches)
+      patient_ids += patient_ids_for_rolling_searches
+    end
+
+    if patient_ids.any?
+      SearchVaccinationRecordsInNHSJob.perform_bulk(patient_ids.zip)
+    end
+  end
+
+  private
+
+  def default_programme_types = ["flu"].freeze
+  def daily_enqueue_size_limit = 1000
+  def rolling_search_period_in_days = 28
+
+  def patient_ids_session_searches(programmes:)
+    scope =
+      Session
+        .includes(:session_dates)
+        .has_programmes(programmes)
+        .where("session_dates.value >= ?", Time.zone.today)
+        .references(:session_dates)
+        .then do
+          it.where("sessions.send_invitations_at <= ?", 2.days.from_now).or(
+            it.where("sessions.send_consent_requests_at <= ?", 2.days.from_now)
+          )
+        end
+
+    scope.find_each.flat_map { |session| session.patients.pluck(:id) }
+  end
+
+  def patient_ids_for_rolling_searches
+    patient_ids ||=
+      Patient
+        .where.not(
+          id:
+            PatientProgrammeVaccinationsSearch.where(
+              "last_searched_at > ?",
+              rolling_search_period_in_days.days.ago
+            ).select(:patient_id)
+        )
+        .ids
+
+    batch_size = (patient_ids.count.to_f / rolling_search_period_in_days).ceil
+    batch_size = [batch_size, daily_enqueue_size_limit].max
+    patient_ids.first(batch_size)
   end
 end
